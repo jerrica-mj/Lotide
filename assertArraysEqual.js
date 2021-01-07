9d118943524cc18bf594ffab1db23c7fb0e9ff5f
JJ <jerrica.mj@gmail.com>
2021-01-07 07:25:07 +0000

    Modified assertArraysEqual function to call eqArrays function, per instructions; modified test cases to run with modified code. -jj

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -24,9 +24,9 @@ const eqArrays = function(array1, array2) {
 
 // ACTUAL FUNCTION
 // Function to utilize eqArrays and assertEqual together
-const assertArraysEqual = function(actual, expected) {
-  // Select resulting message based on whether the actual equality matched the expected assertion
-  const result = (actual === expected) ? (`✅ Assertion Passed: ${actual} === ${expected}`) : (`❌ Assertion Failed: ${actual} !== ${expected}`);
+const assertArraysEqual = function(array1, array2) {
+  // Select resulting message based on whether the arrays are actual equal, as asserted
+  const result = (eqArrays(array1, array2)) ? (`✅ Assertion Passed: [${array1}] === [${array2}]`) : (`❌ Assertion Failed: [${array1}] !== [${array2}]`);
 
   // Log the result
   console.log(result);
@@ -41,7 +41,7 @@ console.log(eqArrays([1, 2, 3], [3, 2, 1])); // false
 console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // true
 console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // false
 
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // should pass
-assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // should pass
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), false); // => should fail
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should fail
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // should pass
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // should fail
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => should pass
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => should fail
