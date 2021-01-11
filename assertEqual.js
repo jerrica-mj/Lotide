101fe52e235cf48dc53685dfea6a1b921425ac3c
JJ <jerrica.mj@gmail.com>
2021-01-11 22:11:03 +0000

    Slight refactoring of function and comments for readability. Commented out test code. Added module export. -jj

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,20 @@
-// a custom assert function (like console.assert()) to log a message to the console
+// Implement a function to test assertion that expected result matches actual
 
 // FUNCTION INITIALIZATION
 const assertEqual = function(actual, expected) {
-  // assign message based on truthy/falsy result
-  const message = (actual === expected) ? (`✅ Assertion Passed: ${actual} === ${expected}`) : (`❌ Assertion Failed: ${actual} !== ${expected}`);
-  console.log(message); /* log the message */
+  // assign message based on strict equality
+  const message = (actual === expected) ?
+    (`✅ Assertion Passed: ${actual} === ${expected}`) :
+    (`❌ Assertion Failed: ${actual} !== ${expected}`);
+  console.log(message); // log the message
+  return message; // use return for testing result
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("hello", "hello");
-assertEqual(3, 6);
+// assertEqual("Lighthouse Labs", "Bootcamp"); // fail
+// assertEqual(1, 1); // pass
+// assertEqual("hello", "hello"); // pass
+// assertEqual(3, 6); // fail
+
+// EXPORT MODULE (file) for use in other files/functions
+module.exports = assertEqual;
