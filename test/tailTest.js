f029ca5b28b9270169ba2dd3db9129f308128fc5
JJ <jerrica.mj@gmail.com>
2021-01-12 04:50:11 +0000

    Refactored tailTest.js to use mocha and chai assertion testing. -jj

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,36 +1,32 @@
 const tail = require("../tail");
-const assertEqual = require("../assertEqual")
+const assert = require("chai").assert;
 
-// TEST CODE WITH SAMPLE/TEST ASSERTIONS
-// test case: arrays can't be compared in assertEqual, because JS doesn't allow === or == to compare arrays
-const failure = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(failure, ["Lighthouse", "Labs"]); // will always FAIL
+// TEST CODE WITH MOCHA AND CHAI
+describe("#tail", () => {
+  // test case 1: check the returned array has all but [0]
+  it('returns ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    // Use assert.deepEqual for comparing arrays
+    assert.deepEqual(result, ["Lighthouse", "Labs"]);
+  });
 
-// test case 1: check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // pass
-assertEqual(result[0], "Lighthouse"); // pass
-assertEqual(result[1], "Labs"); // pass
+  // test case 2: check the original array is unmodified
+  it("the input array remains unchanged", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    // assertEqual(words.length, 3); // pass
+    assert.deepEqual(words, ["Yo Yo", "Lighthouse", "Labs"]);
+  });
 
+  // test case 3: check array of 1 element returns empty tail array
+  it("returns an empty array for an array of 1 element", () => {
+    const lonely = ['solo'];
+    assert.deepEqual(tail(lonely), []);
+  });
 
-// test case 2: check the original array is unmodified
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3); // pass
-
-// test case 3: check array of 1 element returns empty tail array
-const lonely = ['solo'];
-const lonelyTail = tail(lonely);
-assertEqual(lonelyTail.length, 0); // pass
-
-// text case 4: check empty array returns empty tail array
-const empty = [];
-assertEqual(tail(empty).length, 0); // pass
-
-// test case 5: check result is a new array, different use of assertEqual to get closer to comparing the arrays
-const lastOne = [1,4,5,8];
-const lastOneTail = tail(lastOne);
-// tail should have 1 less element than original array--empty in this case
-assertEqual(lastOneTail.length, lastOne.length - 1); // pass
-// first element of tail should be second of original array
-assertEqual(lastOneTail[0], lastOne[1]); // pass
+  // text case 4: check empty array returns empty tail array
+  it("returns an empty tail array for an empty array", () => {
+    const empty = [];
+    assert.deepEqual(tail(empty), []);
+  });
+});
