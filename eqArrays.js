833e214c850e1d3cc92fe799324396c56c40e8bf
JJ <jerrica.mj@gmail.com>
2021-01-12 01:16:12 +0000

    Refactored eqArrays.js to streamline code with module export/require; created eqArraysTest.js to hold test assertions. -jj

--- eqArrays.js
+++ eqArrays.js
@@ -1,54 +1,24 @@
 // Create a function that can compare two arrays for a perfect match.
 
 // FUNCTION INITIALIZATION
-// ASSERTION FUNCTION
-// Function to verify if result matches expected
-const assertEqual = function(actual, expected) {
-  // assign message based on truthy/falsy result
-  const message = (actual === expected) ? (`✅ Assertion Passed: ${actual} === ${expected}`) : (`❌ Assertion Failed: ${actual} !== ${expected}`);
-
-  console.log(message); /* log the message */
-};
-
-
-// ACTUAL FUNCTION
-// Function to compare two arrays for exact match
+// Function to compare two arrays for strict match
 const eqArrays = function(array1, array2) {
-  // Compare array lengths, returning false (exiting function) if mismatched
+  // Compare array lengths, returning false if mismatched
   if (array1.length !== array2.length) {
     return false;
   }
 
   // Compare the elements of the arrays  (if lengths match per above)
   for (let i = 0; i < array1.length; i++) {
-    // If any corresponding elements do not match, return false (exiting function)
+    // If any corresponding elements do not match, return false
     if (array1[i] !== array2[i]) {
       return false;
     }
   }
 
-  // Return true only if above conditions are passed (matching length and elements)
+  // Return true only if lengths and elements match
   return true;
 };
 
 
-
-// TEST CASES
-// Sample cases for eqArrays function
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-console.log(eqArrays([1, 2, 3], [1, 2, 3, 4])); // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-console.log(eqArrays(["a", "b", "c"], ["a", "b", "c"])); // => true
-
-
-console.log(eqArrays([], [])); // => true
-console.log(eqArrays([], [ ])); // => true
-
-// Sample case using eqArrays and assertEqual - test that array equality is true
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should pass
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), false); // => should fail
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should fail
+module.exports = eqArrays;
---
+++ test/eqArraysTest.js
@@ -0,0 +1,22 @@
+const eqArrays = require("../eqArrays");
+const assertEqual = require("../assertEqual");
+
+// TEST CASES
+// Sample cases for eqArrays function
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+console.log(eqArrays([1, 2, 3], [1, 2, 3, 4])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+console.log(eqArrays(["a", "b", "c"], ["a", "b", "c"])); // => true
+
+
+console.log(eqArrays([], [])); // => true
+console.log(eqArrays([], [ ])); // => true
+
+// Sample case using eqArrays and assertEqual - test that array equality is true
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
