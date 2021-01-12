1513e78734696601c0d2d4875586e44f9593a547
JJ <jerrica.mj@gmail.com>
2021-01-12 05:02:49 +0000

    Refactored middleTest.js to use mocha and chai assertion testing. -jj

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,21 @@
 const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
 
-// TEST CODE
-// Test middle function results with assertions
-assertArraysEqual(middle([1]), []); // should pass
-assertArraysEqual(middle([1, 2]), []); // pass
-assertArraysEqual(middle([1, 2, 3]), [2]); // should pass
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // pass
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // pass
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // should fail
+// TEST CODE WITH MOCHA AND CHAI
+describe("#middle", () => {
+  it("returns an empty array for an array with a single element", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it("returns an empty array for an array with 2 elements", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it("returns an array of the middle element for an array with an odd number of elements", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("returns an array of the 2 middle-most elements for an array with an even number of elements", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+});
